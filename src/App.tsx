d7a7e724c394a7483f0bb5fc3111dcfd7440ebe9
futug <102650930+futug@users.noreply.github.com>
2023-11-16 16:36:20 +0600

    trying to fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import Router from "./utilities/Router/Router";
 
 function App() {
-    return <Router />;
+    return (
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/utilities/Router/Router.tsx
+++ src/utilities/Router/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as RouterProvider, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import EntryPage from "../../pages/EntryPage/EntryPage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import RegisterPage from "../../pages/RegiserPage/RegisterPage";
@@ -7,15 +7,13 @@ import NotFoundPage from "../../pages/404/NotFoundPage";
 
 const Router = () => {
     return (
-        <RouterProvider>
-            <Routes>
-                <Route path="/" element={<EntryPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/success" element={<SuccesActivation />} />
-                <Route path="/*" element={<NotFoundPage />} />
-            </Routes>
-        </RouterProvider>
+        <Routes>
+            <Route path="/" element={<EntryPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/success" element={<SuccesActivation />} />
+            <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
     );
 };
 
